b8e720bc3870a91b3a8cfdfd4211e4185acdd613
Chambeus <jwchambers00@gmail.com>
2026-01-23 12:24:48 -0500

    This starts my project via avaloniaui

---
+++ Project-Emulating-My-Childhood/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Project_Emulating_My_Childhood;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
