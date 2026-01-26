cde3c0faa5e068d3f7960acbbbdca7bace411f31
Jordan Chambers <jwchambers00@gmail.com>
2026-01-26 14:15:55 -0500

    I managed to start after all

--- Plugins/Dolphin.Plugin/obj/Debug/net8.0/Dolphin.Plugin.AssemblyInfo.cs
+++ Plugins/Dolphin.Plugin/obj/Debug/net8.0/Dolphin.Plugin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dolphin.Plugin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63609e25fc4dc1c1521ebb0fe99148909ebaa6f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e0830f49a1760eb7916693f0c5e7d0d6773110")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dolphin.Plugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dolphin.Plugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
